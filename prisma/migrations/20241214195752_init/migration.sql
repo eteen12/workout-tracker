63302be3ae26a3582b5aa12de111a53c4dbcbbba
eteen12 <eteensmash@gmail.com>
2024-12-14 14:56:06 -0800

    database overhaul

--- prisma/migrations/20241214195752_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Routine" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "category" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);

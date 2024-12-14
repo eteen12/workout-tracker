441ea40b3398191fd9d19dfc6eb7bbecaca61ead
eteen12 <eteensmash@gmail.com>
2024-12-13 16:46:16 -0800

    person changed to routine

--- components/sections/hero.jsx
+++ components/sections/hero.jsx
@@ -3,45 +3,12 @@ import { ChevronRightIcon } from "@heroicons/react/20/solid"
 import { IoIosList } from "react-icons/io"
 import { IoAdd } from "react-icons/io5"
 
-const people = [
+const routines = [
   {
-    name: "Leslie Alexander",
-    email: "",
-    role: "Co-Founder / CEO",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    href: "#",
-    lastSeen: "3h ago",
-    lastSeenDateTime: "2023-01-23T13:23Z",
-  },
-  {
-    name: "Michael Foster",
-    email: "",
-    role: "Co-Founder / CTO",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    href: "#",
-    lastSeen: "3h ago",
-    lastSeenDateTime: "2023-01-23T13:23Z",
-  },
-  {
-    name: "Dries Vincent",
-    email: "",
-    role: "Business Relations",
-    imageUrl:
-      "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    href: "#",
-    lastSeen: null,
-  },
-  {
-    name: "Lindsay Walton",
-    email: "lindsay.walton@example.com",
-    role: "Front-end Developer",
-    imageUrl:
-      "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    href: "#",
-    lastSeen: "3h ago",
-    lastSeenDateTime: "2023-01-23T13:23Z",
+    category: "BACK",
+    link: "#",
+    description: "asl",
+    workouts: "4",
   },
 ]
 
@@ -66,9 +33,9 @@ export default function Hero() {
           </button>
           <h2 className="text-blue-600">Add New Routine</h2>
         </li>
-        {people.map((person) => (
+        {routines.map((routine) => (
           <li
-            key={person.name}
+            key={routine.link}
             className="relative flex justify-between gap-x-6 px-4 py-2 hover:bg-gray-50 sm:px-6"
           >
             <div className="flex min-w-0 gap-x-4">
@@ -76,40 +43,17 @@ export default function Hero() {
 
               <div className="min-w-0 flex-auto">
                 <p className="text-sm/6 font-semibold text-slate-200">
-                  <a href={person.href}>
+                  <a href={routine.link}>
                     <span className="absolute inset-x-0 -top-px bottom-0" />
-                    {person.name}
+                    {routine.category}
                   </a>
                 </p>
                 <p className="mt-1 flex text-xs/5 text-gray-400">
-                  <a
-                    href={`mailto:${person.email}`}
-                    className="relative truncate hover:underline"
-                  >
-                    {person.email}
-                  </a>
+                  {routine.description}
                 </p>
               </div>
             </div>
             <div className="flex shrink-0 items-center gap-x-4">
-              <div className="hidden sm:flex sm:flex-col sm:items-end">
-                <p className="text-sm/6 text-slate-200">{person.role}</p>
-                {person.lastSeen ? (
-                  <p className="mt-1 text-xs/5 text-gray-400">
-                    Last seen{" "}
-                    <time dateTime={person.lastSeenDateTime}>
-                      {person.lastSeen}
-                    </time>
-                  </p>
-                ) : (
-                  <div className="mt-1 flex items-center gap-x-1.5">
-                    <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                      <div className="size-1.5 rounded-full bg-emerald-500" />
-                    </div>
-                    <p className="text-xs/5 text-gray-400">Online</p>
-                  </div>
-                )}
-              </div>
               <div className="flex">
                 <p className="text-sm text-gray-400">1</p>
                 <ChevronRightIcon

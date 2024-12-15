c734190a8cadbc36ec65eb352175418f15f80f97
eteen12 <eteensmash@gmail.com>
2024-12-14 21:20:07 -0800

    addworkout page started

--- components/workouts/workouts.jsx
+++ components/workouts/workouts.jsx
@@ -53,7 +53,7 @@ export default function Workouts({ id }) {
   return (
     <div className="flex flex-col">
       <div className="px-4">
-        <h1 className="mb-1 text-lg text-slate-200"></h1>
+        <h1 className="mb-1 text-lg text-slate-200">{workout.category}</h1>
       </div>
       <ul
         role="list"
@@ -65,7 +65,7 @@ export default function Workouts({ id }) {
             onDelete={() => handleDelete(thisWorkout.id)}
             classNameTag=""
           >
-            <Link href={`/routine/${thisWorkout.id}`}>
+            <Link href={`/sets/${thisWorkout.id}`}>
               <li className="relative flex justify-between gap-x-6 bg-zinc-900 px-4 py-2">
                 <div className="flex min-w-0 gap-x-4">
                   <div className="min-w-0 flex-auto">

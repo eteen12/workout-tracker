ee84fcdf5832e6030fb4bbdd8cfae50a4ce31c88
eteen12 <eteensmash@gmail.com>
2025-01-01 20:11:28 -0800

    feat: PWA set up complete and configured

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
   output: "export",
+  // compiler: {
+  //   removeConsole: process.env.NODE_ENV !== "development",
+  // },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
